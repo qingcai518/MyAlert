bbc8a2787dc49ed201c2f493710d4b4769bd6ecd
qingcai518 <qingcai518@hotmail.co.jp>
2016-02-07 22:43:18 +0900

    map

--- MyAlert/MapViewController.m
+++ MyAlert/MapViewController.m
@@ -9,8 +9,10 @@
 @import GoogleMaps;
 
 @implementation MapViewController {
-    GMSMapView *mapView_;
+    GMSMapView *mapView;
     NSInteger index;
+    CLLocationCoordinate2D position;
+    GMSMarker *marker;
 }
 
 - (instancetype) init:(NSInteger) pos {
@@ -22,17 +24,32 @@
 }
 
 - (void)viewDidLoad {
-    CLLocationManager *locationManager = LocationManagerSingleton.sharedSingleton.locationManager;
-    CLLocationCoordinate2D position = locationManager.location.coordinate;
-    NSDictionary *dest = LocationManagerSingleton.sharedSingleton.selectStations[index];
+    [super viewDidLoad];
     
-    CGFloat currentZoom = mapView_.camera.zoom;
+    CLLocationManager *locationManager = LocationManagerSingleton.sharedSingleton.locationManager;
+    position = locationManager.location.coordinate;
+
+    // zoom:1: World  5: Landmass/continent  10: City  15: Streets  20: Buildings
     GMSCameraPosition *camera = [GMSCameraPosition cameraWithLatitude:position.latitude
                                                             longitude:position.longitude
-                                                                 zoom:currentZoom];
-    mapView_ = [GMSMapView mapWithFrame:CGRectZero camera:camera];
-    mapView_.myLocationEnabled = YES;
-    self.view = mapView_;
+                                                                 zoom:10];
+    mapView = [GMSMapView mapWithFrame:CGRectZero camera:camera];
+    mapView.myLocationEnabled = YES;
+    self.view = mapView;
+//    [mapView addObserver:self forKeyPath:@"MyLocation" options:NSKeyValueObservingOptionNew context:nil];
+}
+
+-(void)updateMap:(NSNotification*)notification {
+    //update current location
+//    CLLocationManager *locationManager = LocationManagerSingleton.sharedSingleton.locationManager;
+//    position = locationManager.location.coordinate;
+//    GMSCoordinateBounds *bounds = [[GMSCoordinateBounds alloc] initWithCoordinate:position coordinate:position];
+//    bounds = [bounds includingCoordinate:marker.position];
+//    [mapView animateWithCameraUpdate:[GMSCameraUpdate fitBounds:bounds withPadding:70.0f]];
+}
+
+- (void) viewDidAppear:(BOOL)animated {
+    NSDictionary *dest = LocationManagerSingleton.sharedSingleton.selectStations[index];
     
     double latitude = [[dest valueForKey:@"latitude"] doubleValue];
     double longitude = [[dest valueForKey:@"longitude"] doubleValue];
@@ -41,11 +58,11 @@
     NSString *line = [dest valueForKey:@"line"];
     
     // Creates a marker in the center of the map.
-    GMSMarker *marker = [[GMSMarker alloc] init];
+    marker = [[GMSMarker alloc] init];
     marker.position = CLLocationCoordinate2DMake(latitude, longitude);
     marker.title = name;
     marker.snippet = [NSString stringWithFormat:@"%@ %@", line, place];
-    marker.map = mapView_;
+    marker.map = mapView;
     
     // add polyline
     GMSMutablePath *path = [GMSMutablePath path];
@@ -53,7 +70,12 @@
     [path addCoordinate:CLLocationCoordinate2DMake(latitude, longitude)];
     GMSPolyline *polyline = [GMSPolyline polylineWithPath:path];
     polyline.strokeColor = [UIColor redColor];
-    polyline.map = mapView_;
+    polyline.strokeWidth = 5;
+    polyline.map = mapView;
+    
+    GMSCoordinateBounds *bounds = [[GMSCoordinateBounds alloc] initWithCoordinate:position coordinate:position];
+    bounds = [bounds includingCoordinate:marker.position];
+    [mapView animateWithCameraUpdate:[GMSCameraUpdate fitBounds:bounds withPadding:70.0f]];
 }
 
 @end
